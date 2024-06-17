c04a04043abeee10c65b9b1a66a665f012a578cd
Rahul Sharma <rahulgaur116@gmail.com>
2024-06-17 19:45:43 -0400

    added user and thought reaction controller js within controller folder

---
+++ controllers/thoughtController.js
@@ -0,0 +1,121 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // GET all Thoughts
+  getThoughts(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // GET Thought by ID
+  thoughtById(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .then((dbThoughtData) => {
+        !dbThoughtData
+          ? res.status(404).json({
+              message: "Thought does not exist",
+            })
+          : res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log("An error has occurred:", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // POST a Thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findByIdAndUpdate(
+          req.body.userId,
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // UPDATE a Thought
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((updatedThought) => {
+        if (!updatedThought) {
+          return res.status(404).json({ message: "No thought with this ID!" });
+        } else {
+          res.json(updatedThought);
+        }
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // DELETE a Thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.id })
+      .then((dbThoughtData) => {
+        !dbThoughtData
+          ? res.status(404).json({
+              message: "Error: Thought does not exist.",
+            })
+          : res.status(200).json({
+              message: "Thought deleted successfully.",
+            });
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // POST a reaction
+  createReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        !dbThoughtData
+          ? res.status(404).json({ message: "No thought found with this ID!" })
+          : res.json({
+              message: "Successfully added reaction",
+              dbThoughtData,
+            });
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // DELETE a reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } }
+    )
+      .then((dbThoughtData) => {
+        !dbThoughtData
+          ? res.status(404).json({
+              message: "Error: Thought does not exist.",
+            })
+          : res.status(200).json({
+              message: "Thought deleted successfully.",
+            });
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,137 @@
+const { Thought, User } = require("../models");
+
+const userController = {
+  // GET all Users
+  getUsers(req, res) {
+    User.find({})
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // GET User by ID
+  userById(req, res) {
+    User.findOne({ _id: req.params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // POST a User
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // DELETE a User && DELETE associated thought(s)
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({
+            message: "Error: User does not exist.",
+          });
+        }
+        Thought.deleteMany({username: dbUserData.username})
+        .then((result) => {
+          res.status(200).json({
+            message: "User deleted successfully.",
+          });
+        })
+        .catch((thoughtsError) => {
+          res.status(500).json({
+            message: "An error occured when deleting thoughts"
+          });
+        });
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // UPDATE a User
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({
+            message: "Error: User does not exist.",
+          });
+        } else {
+          res.status(200).json({
+            message: "User updated successfully.",
+            user: dbUserData,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // ADD a friend
+  addFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.id,
+      { $push: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((dbFriendData) => {
+        if (!dbFriendData) {
+          res.status(404).json({
+            message: "Error: User does not exist.",
+          });
+        } else {
+          res.status(200).json({
+            message: "Friends updated successfully.",
+            user: dbFriendData,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+
+  // REMOVE a friend
+  deleteFriend(req, res) {
+    User.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((dbFriendData) => {
+        if (!dbFriendData) {
+          res.status(404).json({
+            message: "Error: User does not exist.",
+          });
+        } else {
+          res.status(200).json({
+            message: "Friend deleted successfully.",
+            user: dbFriendData,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("An error has occurred: ", err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
